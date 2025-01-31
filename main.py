650661d9fff99b27338ad7a6805839a26e18ea93
cozzi47 <cozzi47@hotmail.com>
2025-01-31 02:01:20 -0500

    add player.rotate, player.update

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -18,9 +15,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+        
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
